f3efd036217fce7a2d412b73f45a667a4042f765
Xiliang Chen <xlchen1291@gmail.com>
2013-09-04 23:55:17 +1200

    Initial Commit

---
+++ XLCUtils/XLCUtils.h
@@ -0,0 +1,13 @@
+//
+//  XLCUtils.h
+//  XLCUtils
+//
+//  Created by Xiliang Chen on 13-9-4.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLCUtils : NSObject
+
+@end

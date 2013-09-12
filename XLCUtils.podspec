58d15ac67360709d70a9b768558b229c81ccc903
Xiliang Chen <xlchen1291@gmail.com>
2013-09-12 22:05:34 +1200

    add MemoryDebug and other utils

--- XLCUtils.podspec
+++ XLCUtils.podspec
@@ -2,13 +2,20 @@ Pod::Spec.new do |s|
   s.name         = "XLCUtils"
   s.version      = "0.0.1"
   s.summary      = "Some Objective-C Utilities"
-  s.description  = <<-DESC
-                  Some Objective-C Utilities.
-                  DESC
   s.homepage     = "https://github.com/xlc/XLCUtils"
   s.license      = 'MIT'
   s.author       = { "Xiliang Chen" => "xlchen1291@gmail.com" }
   s.source       = { :git => "https://github.com/xlc/XLCUtils.git", :commit => "fb9216ee32adc9e668dc9d06dbda3cf05416f596" }
-  s.source_files  = 'XLCUtils', 'XLCUtils/**/*.{h,m,mm}'
+  s.source_files  = 'XLCUtils'
   s.requires_arc = true
+
+  s.ios.deployment_target = '6.0'
+
+  s.osx.deployment_target = '10.8'
+  
+  s.subspec 'no-arc' do |sp|
+    sp.source_files = 'XLCUtils/NSObject+XLCUtilsMemoryDebug.m'
+    sp.requires_arc = false
+  end
+
 end
---
+++ XLCUtils/NSObject+XLCUtilsMemoryDebug.h
@@ -0,0 +1,40 @@
+//
+//  NSObject+XLCUtilsMemoryDebug.h
+//  XLCUtils
+//
+//  Created by Xiliang Chen on 13-9-12.
+//  Copyright (c) 2013年 Xiliang Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (XLCTestUtilsMemoryDebug)
+
+/**
+ * Perform isa-swizzle to override retain / release / autorelease.
+ */
+- (id)xlc_swizzleRetainRelease;
+
+/**
+ * Restore isa.
+ */
+- (void)xlc_restoreRetainRelease;
+
+/**
+ * Count of autorelease called.
+ * This is can be useful to calculate "real" retain count given no
+ * autorelease pool was drain (which is the typical case when unit testing).
+ */
+- (NSUInteger)xlc_autoreleaseCount;
+
+// stub methods
+- (id)xlc_retain;
+- (void)xlc_release;
+- (id)xlc_autorelease;
+- (Class)xlc_class;
+- (void)xlc_dealloc;
+
+// not swizzled, just call original retainCount
+- (NSUInteger)xlc_retainCount;
+
+@end

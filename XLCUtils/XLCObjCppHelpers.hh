5b3d4d0b07fa09a5c2be286ad3eb46225a5d83b5
Xiliang Chen <xlchen1291@gmail.com>
2014-04-27 18:25:32 +1200

    added some ObjC++ helpers

---
+++ XLCUtils/XLCObjCppHelpers.hh
@@ -0,0 +1,148 @@
+//
+//  XLCObjCppHelpers.hh
+//  XLCUtils
+//
+//  Created by Xiliang Chen on 14-4-27.
+//  Copyright (c) 2014年 Xiliang Chen. All rights reserved.
+//
+
+#ifndef XLCUtils_XLCObjCppHelpers_hh
+#define XLCUtils_XLCObjCppHelpers_hh
+
+#include <type_traits>
+#include <ostream>
+#include <utility>
+#include <string>
+#include <typeinfo>
+
+#include <cxxabi.h>
+
+#ifndef __has_builtin           // Optional of course.
+#define __has_builtin(x) 0      // Compatibility with non-clang compilers.
+#endif
+#ifndef __has_feature           // Optional of course.
+#define __has_feature(x) 0      // Compatibility with non-clang compilers.
+#endif
+#ifndef __has_extension
+#define __has_extension __has_feature // Compatibility with pre-3.0 compilers.
+#endif
+
+namespace xlc {
+    
+    template <bool T, class U = void>
+    using enable_if_t = typename std::enable_if<T, U>::type;
+    
+    // callable_traits
+    
+    namespace detail {
+        template <class ReturnType, class... Args>
+        struct callable_traits_base
+        {
+            using return_type = ReturnType;
+            using argument_type = std::tuple<Args...>;
+            
+            template<std::size_t I>
+            using arg = typename std::tuple_element<I, argument_type>::type;
+        };
+    }
+    
+    template <class T>
+    struct callable_traits : callable_traits<decltype(&T::operator())>
+    {};
+    
+    // lambda / functor
+    template <class ClassType, class ReturnType, class... Args>
+    struct callable_traits<ReturnType(ClassType::*)(Args...) const>
+    : detail::callable_traits_base<ReturnType, Args...>
+    {};
+    
+    // function pointer
+    template <class ReturnType, class... Args>
+    struct callable_traits<ReturnType(Args...)>
+    : detail::callable_traits_base<ReturnType, Args...>
+    {};
+    
+    template <class ReturnType, class... Args>
+    struct callable_traits<ReturnType(*)(Args...)>
+    : detail::callable_traits_base<ReturnType, Args...>
+    {};
+    
+#if __has_extension(blocks)
+    // block
+    template <class ReturnType, class... Args>
+    struct callable_traits<ReturnType(^)(Args...)>
+    : detail::callable_traits_base<ReturnType, Args...>
+    {};
+#endif
+    
+    // is_for_loopable
+    
+    namespace detail {
+        namespace is_for_loopable {
+            using std::begin;   // enable ADL
+            
+            template<class T, class V = void>
+            struct is_for_loopable : std::false_type { };
+            
+            template<class T>
+            struct is_for_loopable<T,
+            typename xlc::enable_if_t<!std::is_void<decltype(begin(std::declval<T>()))>::value>
+            > : std::true_type { };
+            
+            template<class T, std::size_t N>
+            struct is_for_loopable<T[N]> : std::true_type { };
+        }
+    }
+    
+    using detail::is_for_loopable::is_for_loopable;
+    
+    // is_objc_class
+    
+    template<class T, class V = void>
+    struct is_objc_class : std::false_type { };
+    
+#ifdef __OBJC__
+    template<class T>
+    struct is_objc_class<T,
+    typename std::enable_if<std::is_convertible<T, id>::value>::type
+    > : std::true_type { };
+#endif
+    
+    // type_description
+    
+    template <class T>
+    std::string type_description() {
+        bool is_lvalue_reference = std::is_lvalue_reference<T>::value;
+        bool is_rvalue_reference = std::is_rvalue_reference<T>::value;
+        bool is_const = std::is_const<typename std::remove_reference<T>::type>::value;
+        
+        std::unique_ptr<char, void(*)(void*)>
+        name {abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, nullptr), std::free};
+        
+        auto str = std::string(name.get());
+        if (is_const) {
+            str += " const";
+        }
+        if (is_lvalue_reference) {
+            str += "&";
+        }
+        if (is_rvalue_reference) {
+            str += "&&";
+        }
+        
+        return str;
+    };
+}
+
+#ifdef __OBJC__
+template <
+class T,
+class = typename xlc::enable_if_t<xlc::is_objc_class<T>::value>
+>
+std::ostream& operator<< (std::ostream& stream, T const & t) {
+    stream << [[t description] UTF8String];
+    return stream;
+}
+#endif
+
+#endif
---
+++ XLCUtilsTests/XLCObjCppHelpersTests.mm
@@ -0,0 +1,164 @@
+//
+//  XLCObjCppTests.m
+//  XLCUtils
+//
+//  Created by Xiliang Chen on 14-4-26.
+//  Copyright (c) 2014年 Xiliang Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#include <vector>
+#include <deque>
+#include <sstream>
+
+#include "XLCObjCppHelpers.hh"
+
+@interface XLCObjCppTests : XCTestCase
+
+@end
+
+@implementation XLCObjCppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testStreamOperator
+{
+    std::stringstream stream;
+    stream << @"test" << "cstr" << @1 << 2;
+    XCTAssertEqualObjects(@(stream.str().c_str()), @"testcstr12");
+}
+    
+- (void)testTypeDescription
+{
+    XCTAssertEqualObjects(@(xlc::type_description<int>().c_str()), @"int");
+    XCTAssertEqualObjects(@(xlc::type_description<int&>().c_str()), @"int&");
+    XCTAssertEqualObjects(@(xlc::type_description<int&&>().c_str()), @"int&&");
+    XCTAssertEqualObjects(@(xlc::type_description<int const>().c_str()), @"int const");
+    XCTAssertEqualObjects(@(xlc::type_description<int const&>().c_str()), @"int const&");
+    XCTAssertEqualObjects(@(xlc::type_description<int const*>().c_str()), @"int const*");
+    XCTAssertEqualObjects(@(xlc::type_description<int const*&>().c_str()), @"int const*&");
+}
+
+@end
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunneeded-internal-declaration"
+
+namespace test_callable_traits
+{
+    
+    namespace lambda
+    {
+        auto f = [](){};
+        using traits = typename xlc::callable_traits<decltype(f)>;
+        static_assert(std::is_same<traits::return_type, void>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<>>::value, "");
+    }
+    
+    namespace lambda2
+    {
+        auto f = [](double, int){return true;};
+        using traits = typename xlc::callable_traits<decltype(f)>;
+        static_assert(std::is_same<traits::return_type, bool>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<double, int>>::value, "");
+        static_assert(std::is_same<traits::arg<0>, double>::value, "");
+        static_assert(std::is_same<traits::arg<1>, int>::value, "");
+    }
+    
+    namespace function
+    {
+        void f(void) {  }
+        using traits = typename xlc::callable_traits<decltype(f)>;
+        static_assert(std::is_same<traits::return_type, void>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<>>::value, "");
+    }
+    
+    namespace function2
+    {
+        bool f(int) { return true; }
+        using traits = typename xlc::callable_traits<decltype(f)>;
+        static_assert(std::is_same<traits::return_type, bool>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<int>>::value, "");
+        static_assert(std::is_same<traits::arg<0>, int>::value, "");
+    }
+    
+    namespace function_pointer
+    {
+        void f(void) {  }
+        using traits = typename xlc::callable_traits<decltype(&f)>;
+        static_assert(std::is_same<traits::return_type, void>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<>>::value, "");
+    }
+    
+    namespace function_pointer2
+    {
+        bool f(int) { return true; }
+        using traits = typename xlc::callable_traits<decltype(&f)>;
+        static_assert(std::is_same<traits::return_type, bool>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<int>>::value, "");
+        static_assert(std::is_same<traits::arg<0>, int>::value, "");
+    }
+    
+    namespace block
+    {
+        void (^f)(void);
+        using traits = typename xlc::callable_traits<decltype(f)>;
+        static_assert(std::is_same<traits::return_type, void>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<>>::value, "");
+    }
+    
+    namespace block2
+    {
+        bool (^f)(double, int);
+        using traits = typename xlc::callable_traits<decltype(f)>;
+        static_assert(std::is_same<traits::return_type, bool>::value, "");
+        static_assert(std::is_same<traits::argument_type, std::tuple<double, int>>::value, "");
+        static_assert(std::is_same<traits::arg<0>, double>::value, "");
+        static_assert(std::is_same<traits::arg<1>, int>::value, "");
+    }
+    
+}
+
+namespace test_is_for_loopable
+{
+    static_assert(xlc::is_for_loopable<std::vector<int>>::value, "");
+    static_assert(xlc::is_for_loopable<std::deque<int>>::value, "");
+    static_assert(!xlc::is_for_loopable<int>::value, "");
+    static_assert(!xlc::is_for_loopable<void>::value, "");
+    static_assert(!xlc::is_for_loopable<int*>::value, "");
+
+    static_assert(xlc::is_for_loopable<int(&)[4]>::value, "array reference is loopable");
+    static_assert(xlc::is_for_loopable<int[4]>::value, "array is loopable");
+    
+    struct Foo {};
+    int *begin(Foo foo);
+    int *end(Foo foo);
+    
+    static_assert(xlc::is_for_loopable<Foo>::value, "should use ADL to find begin/end");
+}
+
+namespace test_is_objc_class
+{
+    static_assert(xlc::is_objc_class<id>::value, "");
+    static_assert(xlc::is_objc_class<NSObject *>::value, "");
+    static_assert(xlc::is_objc_class<NSString *>::value, "");
+    static_assert(xlc::is_objc_class<NSProxy *>::value, "");
+    static_assert(!xlc::is_objc_class<std::vector<int>>::value, "");
+    static_assert(!xlc::is_objc_class<std::deque<int>>::value, "");
+    static_assert(!xlc::is_objc_class<int>::value, "");
+    static_assert(!xlc::is_objc_class<void>::value, "");
+}
+
+#pragma clang diagnostic pop
+
